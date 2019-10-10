a90186e70a37f0681c98e3bb14a72b87a35d4950
BrandyBecker <BrandyBeckerFSW@Gmail.com>
2019-10-10 16:04:40 -0400

    + Styling!

--- app/src/components/CatCard.js
+++ app/src/components/CatCard.js
@@ -3,7 +3,7 @@ import {Card} from 'react-bootstrap';
 
 const CatCard = (props) => {
     return (
-        <Card style={{ width: '20rem', height: '18rem' }}>
+        <Card className="singleCard" style={{ width: '20rem' }}>
             <Card.Img variant="top" src={props.data.url} />
         
         </Card>
